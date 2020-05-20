553030f5c2fd4b6d24f5dfc42847264a7f8eb7b0
Ole Algoritme <olealgoritme@gmail.com>
2020-05-20 23:58:03 +0200

    Update w10.sh

--- w10.sh
+++ w10.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Pass-thru GPUs
-# Shared webdav folder with spice
+# virtio-win ISO + win10 qcow2 image
 
 qemu-system-x86_64 \
     -machine q35,accel=kvm \
     -cpu host,kvm=off,topoext=on,hv_vendor_id=sugkuk \
     -enable-kvm \
     -m 8192 \
-	-device vfio-pci,host=4e:00.0,bus=pcie.0,multifunction=on,x-vga=on \
-	-device vfio-pci,host=4e:00.1,bus=pcie.0 \
-	-device vfio-pci,host=4e:00.2,bus=pcie.0 \
-	-device vfio-pci,host=4e:00.3,bus=pcie.0 \
+    -device vfio-pci,host=4e:00.0,bus=pcie.0,multifunction=on,x-vga=on \
+    -device vfio-pci,host=4e:00.1,bus=pcie.0 \
+    -device vfio-pci,host=4e:00.2,bus=pcie.0 \
+    -device vfio-pci,host=4e:00.3,bus=pcie.0 \
     -device vfio-pci,host=49:00.0,bus=pcie.0 \
     -rtc base=localtime,clock=host,driftfix=slew \
     -smp 16,sockets=1,cores=16,threads=1 \
@@ -23,4 +23,3 @@ qemu-system-x86_64 \
     -object memory-backend-file,id=ivshmem,share=on,mem-path=/dev/shm/looking-glass,size=64M \
     -spice port=5900,disable-ticketing \
     -vga qxl \
-    -device virtserialport,bus=virtio-serial0.0,nr=1,chardev=charchannel1,id=channel1,name=org.spice-space.webdav.0 -chardev spiceport,name=org.spice-space.webdav.0,id=charchannel1 \
